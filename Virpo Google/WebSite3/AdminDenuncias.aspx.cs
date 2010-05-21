f677f902202441ae2f71734dce865f4f19b6084b
lucianomgomez <lucianomgomez@367e1e5c-9c90-11de-b798-49b661d4457b>
2010-05-21 22:26:19 +0000

    Denuncias WikiMusic

--- Virpo Google/WebSite3/AdminDenuncias.aspx.cs
+++ Virpo Google/WebSite3/AdminDenuncias.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
@@ -10,12 +11,39 @@ using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Xml.Linq;
+using CapaNegocio.Entities;
+using CapaNegocio.Factories;
 
 public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
         if (!Page.IsPostBack)
+        {
             if (Session["UsuarioAdmin"] == null) Response.Redirect("admin.aspx");
+            List<Denuncia> denuncias = new List<Denuncia>();
+            denuncias = DenunciaFactory.DevolverTodos();
+            if (denuncias.Count > 0) cargarTabla(denuncias);
+            else lblListadoDenuncias.Text = "No hay Denuncias reportadas.";
+        }
+    }
+
+    private void cargarTabla(List<Denuncia> denuncias)
+    {
+        string html = "";
+        foreach (Denuncia denuncia in denuncias)
+        {
+            html += "<tr>";
+            html += "    <td style='width: 50px' align='center'><a href='PerfilPublico.aspx?Id=" + denuncia.IdDenunciante.ToString() + "'>";
+            html += "<img title='" + denuncia.UsrDenunciante + "' src='ImagenesUsuario/" + ((Usuario)UsuarioFactory.Devolver(denuncia.UsrDenunciante)).Imagen + "'/></a>";
+            html += "<br />" + denuncia.UsrDenunciante + "</td>";
+            html += "    <td style='width: 479px' align='left'>" + "Nro. de Denuncia: " + denuncia.Id.ToString();
+            html += "<br />Fecha: " + denuncia.Fecha.ToString();
+            html += "<br />Sección: " + denuncia.Tipo;
+            html += "<br />Documento Denunciado: <a class='feed-link' href='" + denuncia.Url + "' target='_blank' type='application/atom+xml'>" + denuncia.Descripcion + "</a>";
+            html += "</td>";
+            html += "</tr>";
+        }
+        lblListadoDenuncias.Text = html;
     }
 }
